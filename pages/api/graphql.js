5f27ba578fdb5cb6506573e0df69bd24f2d5498b
Alex Bolbat <a.s.bolbat@gmail.com>
2020-05-10 15:25:44 +0300

    clean up

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -6,8 +6,5 @@ import root from '../../src/graphql/resolvers';
 export default async (req, res) => {
   const query = req.body.query;
   const response = await graphql(schema, query, root);
-  console.log(JSON.stringify(response));
-  //console.log(res.setHeaders('Content-Type', 'application/json'));
-  //return res.end(JSON.stringify(response));
   return res.json(response);
 };
